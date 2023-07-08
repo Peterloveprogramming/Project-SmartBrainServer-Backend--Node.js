2adc4e7a1a414d173321cc9847d0b591f2f248e9
bustop69 <y.peter341@gmail.com>
2023-07-08 17:53:23 +1000

    making changes

--- server.js
+++ server.js
@@ -12,10 +12,12 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
   client: 'pg',
+  connection:{
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  }
+    }
+  }  
 });
 
 
